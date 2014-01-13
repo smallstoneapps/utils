7f10aa3b0f24843d19708248c75a6897e501a005
Matthew Tole <matthewtole@gmail.com>
2014-01-13 21:10:08 +0000

    Add new features from Neal's modifications.
    https://github.com/Skipstone/Skipstone/blob/master/src/libs/pebble-assist.h

--- pebble-assist.h
+++ pebble-assist.h
@@ -1,8 +1,8 @@
 /***
  * Pebble Assist
- * Copyright (C) 2013 Matthew Tole
+ * Copyright (C) 2014 Matthew Tole
  *
- * Version 0.1.0
+ * Version 0.2.0
  ***/
 
 /**
@@ -31,6 +31,8 @@ THE SOFTWARE.
 
 #pragma once
 
+// Missing Constants
+
 #ifndef MENU_CELL_BASIC_CELL_HEIGHT
 #define MENU_CELL_BASIC_CELL_HEIGHT 44
 #endif
@@ -46,6 +48,13 @@ THE SOFTWARE.
 #ifndef STATUS_HEIGHT
 #define STATUS_HEIGHT 16
 #endif
+
+// Shorthand App Logging
+#define INFO(...) app_log(APP_LOG_LEVEL_INFO, __FILE__, __LINE__, __VA_ARGS__)
+#define LOG(...) app_log(APP_LOG_LEVEL_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+#define WARN(...) app_log(APP_LOG_LEVEL_WARNING, __FILE__, __LINE__, __VA_ARGS__)
+#define ERROR(...) app_log(APP_LOG_LEVEL_ERROR, __FILE__, __LINE__, __VA_ARGS__)
+
 
 // Layer Helpers
 #define layer_create_fullscreen(window) layer_create(layer_get_bounds(window_get_root_layer(window)));
@@ -75,6 +84,7 @@ THE SOFTWARE.
 // Menu Layer Helpers
 #define menu_layer_create_fullscreen(window) menu_layer_create(layer_get_bounds(window_get_root_layer(window)))
 #define menu_layer_add_to_window(layer, window) layer_add_child(window_get_root_layer(window), menu_layer_get_layer(layer))
+#define menu_layer_reload_data_and_mark_dirty(layer) menu_layer_reload_data(layer); layer_mark_dirty(menu_layer_get_layer(layer));
 
 // Simple Menu Layer Helpers
 #define simple_menu_layer_create_fullscreen(window, sections, num_sections, callback_context) simple_menu_layer_create(layer_get_bounds(window_get_root_layer(window)), sections, num_sections, callback_context)
@@ -92,3 +102,8 @@ THE SOFTWARE.
 
 // App Message Helpers
 #define app_message_open_max() app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum())
+
+// Persistence Helpers
+#define persist_read_int_safe(key, value) if (persist_exists(key)) { return persist_read_int(key); } else { return value; }
+#define persist_read_bool_safe(key, value) if (persist_exists(key)) { return persist_read_bool(key); } else { return value; }
+
