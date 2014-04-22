57ec32e53b2e03e698a2c2c4bbb4dcdc5a2a7505
Matthew Tole <matthewtole@gmail.com>
2014-04-22 09:58:59 +0100

    Add *_safe wrappers for destroy and free type functions.

--- pebble-assist.h
+++ pebble-assist.h
@@ -2,7 +2,7 @@
  * Pebble Assist
  * Copyright (C) 2014 Matthew Tole
  *
- * Version 0.2.0
+ * Version 0.2.1
  ***/
 
 /**
@@ -50,21 +50,29 @@ THE SOFTWARE.
 #endif
 
 // Shorthand App Logging
-#define INFO(...) app_log(APP_LOG_LEVEL_INFO, __FILE__, __LINE__, __VA_ARGS__)
 #define LOG(...) app_log(APP_LOG_LEVEL_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+#define DEBUG(...) app_log(APP_LOG_LEVEL_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+#define INFO(...) app_log(APP_LOG_LEVEL_INFO, __FILE__, __LINE__, __VA_ARGS__)
 #define WARN(...) app_log(APP_LOG_LEVEL_WARNING, __FILE__, __LINE__, __VA_ARGS__)
 #define ERROR(...) app_log(APP_LOG_LEVEL_ERROR, __FILE__, __LINE__, __VA_ARGS__)
 
+// Window Helpers
+#define window_destroy_safe(window) if (NULL != window) { window_destroy(window); window = NULL; }
+
+// Number Window Helpers
+#define number_window_destroy_safe(window) if (NULL != window) { number_window_destroy(window); window = NULL; }
 
 // Layer Helpers
 #define layer_create_fullscreen(window) layer_create(layer_get_bounds(window_get_root_layer(window)));
 #define layer_add_to_window(layer, window) layer_add_child(window_get_root_layer(window), layer)
 #define layer_show(layer) layer_set_hidden(layer, false)
 #define layer_hide(layer) layer_set_hidden(layer, true)
+#define layer_destroy_safe(layer) if (NULL != layer) { layer_destroy(layer); layer = NULL; }
 
 // Scroll Layer Helpers
 #define scroll_layer_create_fullscreeen(window) scroll_layer_create(layer_get_bounds(window_get_root_layer(window)))
 #define scroll_layer_add_to_window(layer, window) layer_add_child(window_get_root_layer(window), scroll_layer_get_layer(layer))
+#define scroll_layer_destroy_safe(layer) if (NULL != layer) { scroll_layer_destroy(layer); layer = NULL; }
 
 // Text Layer Helpers
 #define text_layer_create_fullscreen(window) text_layer_create(layer_get_bounds(window_get_root_layer(window)));
@@ -72,27 +80,36 @@ THE SOFTWARE.
 #define text_layer_set_system_font(layer, font) text_layer_set_font(layer, fonts_get_system_font(font))
 #define text_layer_set_colours(layer, foreground, background) text_layer_set_text_color(layer, foreground); text_layer_set_background_color(layer, background)
 #define text_layer_set_colors(layer, foreground, background) text_layer_set_text_color(layer, foreground); text_layer_set_background_color(layer, background)
+#define text_layer_destroy_safe(layer) if (NULL != layer) { text_layer_destroy(layer); layer = NULL; }
 
 // Bitmap Layer Helpers
 #define bitmap_layer_create_fullscreen(window) bitmap_layer_create(layer_get_bounds(window_get_root_layer(window)));
 #define bitmap_layer_add_to_window(layer, window) layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(layer))
+#define bitmap_layer_destroy_safe(layer) if (NULL != layer) { bitmap_layer_destroy(layer); layer = NULL; }
 
 // Inverter Layer Helpers
 #define inverter_layer_add_to_window(layer, window) layer_add_child(window_get_root_layer(window), inverter_layer_get_layer(layer))
 #define inverter_layer_create_fullscreen(layer, window) inverter_layer_create(layer_get_bounds(window_get_root_layer(window)))
+#define inverter_layer_destroy_safe(layer) if (NULL != layer) { inverter_layer_destroy(layer); layer = NULL; }
 
 // Menu Layer Helpers
 #define menu_layer_create_fullscreen(window) menu_layer_create(layer_get_bounds(window_get_root_layer(window)))
 #define menu_layer_add_to_window(layer, window) layer_add_child(window_get_root_layer(window), menu_layer_get_layer(layer)); menu_layer_set_click_config_onto_window(layer, window)
 #define menu_layer_reload_data_and_mark_dirty(layer) menu_layer_reload_data(layer); layer_mark_dirty(menu_layer_get_layer(layer));
+#define menu_layer_destroy_safe(layer) if (NULL != layer) { menu_layer_destroy(layer); layer = NULL; }
 
 // Simple Menu Layer Helpers
 #define simple_menu_layer_create_fullscreen(window, sections, num_sections, callback_context) simple_menu_layer_create(layer_get_bounds(window_get_root_layer(window)), sections, num_sections, callback_context)
 #define simple_menu_layer_add_to_window(layer, window) layer_add_child(window_get_root_layer(window), simple_menu_layer_get_layer(layer))
+#define simple_menu_layer_destroy_safe(layer) if (NULL != layer) { simple_menu_layer_destroy(layer); layer = NULL; }
 
 // Action Bar Layer Helpers
 #define action_bar_layer_create_in_window(action_bar, window) action_bar = action_bar_layer_create(); action_bar_layer_add_to_window(action_bar, window)
 #define action_bar_layer_clear_icons(action_bar) action_bar_layer_clear_icon(action_bar, BUTTON_ID_SELECT); action_bar_layer_clear_icon(action_bar, BUTTON_ID_DOWN); action_bar_layer_clear_icon(action_bar, BUTTON_ID_UP)
+#define action_bar_layer_destroy_safe(layer) if (NULL != layer) { action_bar_layer_destroy(layer); layer = NULL; }
+
+// Dynamic Memory Helpers
+#define free_safe(ptr) if (NULL != ptr) { free(ptr); ptr = NULL; }
 
 // App Timer Helpers
 #define app_timer_cancel_safe(timer) if (timer != NULL) { app_timer_cancel(timer); timer = NULL; }
